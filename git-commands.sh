64fe17945d4036ac1ca318b0fae989bcfbccfca7
Sarah Lyne <sarah.lyne@tui.co.uk>
2021-10-21 14:15:16 +0000

    adding new files

--- git-commands.sh
+++ git-commands.sh
@@ -4,13 +4,13 @@
 git add .
 
 # pause
-wait 30
+sleep 5s
 
 # commit files and update README.md with message
 git commit -m "adding new files"
 
 # pause
-wait 30
+sleep 5s
 
 # push files to git
 git push
