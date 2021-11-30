eb90bd58d301504870ff525a7955d8774a599611
sarahlyne46 <86782977+sarahlyne46@users.noreply.github.com>
2021-11-30 09:11:21 +0000

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=tag-name .
+docker build --tag=api .
 
 # Step 2: 
 # List docker images
@@ -12,5 +12,5 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 tag-name
+docker run -p 8000:80 api
 
